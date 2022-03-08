62ba8e62ba0bdb4725cf1b5aa60bffbc378b515a
Nancy <nancygupta@iitbhilai.ac.in>
2022-03-08 19:47:31 +0530

    imported modules

---
+++ Pacman/enemies.py
@@ -0,0 +1,12 @@
+import pygame
+import random
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 576
+
+# Define some colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (0,0,255)
+GREEN = (0,255,0)
+RED = (255,0,0)
