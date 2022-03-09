f6bc086717d4b172c163933482e82944844ac976
Nancy <nancygupta@iitbhilai.ac.in>
2022-03-09 19:10:56 +0530

    adding background to enemy file

--- Pacman/enemies.py
+++ Pacman/enemies.py
@@ -10,3 +10,26 @@ WHITE = (255,255,255)
 BLUE = (0,0,255)
 GREEN = (0,255,0)
 RED = (255,0,0)
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self,x,y,color,width,height):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
+
+class Ellipse(pygame.sprite.Sprite):
+    def __init__(self,x,y,color,width,height):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+        # Set the background color and set it to be transparent
+        self.image = pygame.Surface([width,height])
+        self.image.fill(BLACK)
+        self.image.set_colorkey(BLACK)
+        # Draw the ellipse
+        pygame.draw.ellipse(self.image,color,[0,0,width,height])
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
