0079359bd2a54defd256aa3dd7cf150e697edfc8
srinidhi <you@example.com>
2022-03-09 19:04:22 +0530

    imported modules required

---
+++ Pacman/game.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pygame
+from player import Player
+from enemies import *
+import tkinter
+from tkinter import messagebox
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 576
+
+# Define some colors
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (0,0,255)
+RED = (255,0,0)
+
