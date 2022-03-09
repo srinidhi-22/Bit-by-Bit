a13bc04c067e93937512212782298274d99fb666
srinidhi <you@example.com>
2022-03-09 21:08:08 +0530

    Displaying game window function added

--- Pacman/main.py
+++ Pacman/main.py
@@ -1,2 +1,18 @@
 import pygame
 from game import Game
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 576
+
+def main():
+    # Initialize all imported pygame modules
+    pygame.init()
+    # Set the width and height of the screen [width, height]
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    # Set the current window caption
+    pygame.display.set_caption("PACMAN")
+    #Loop until the user clicks the close button.
+    done = False
+    # Used to manage how fast the screen updates
+    clock = pygame.time.Clock()
+    # Create a game object
+    game = Game()
