1a501150f8e338aba68ae8097bd77e79d89d7143
Your Name <you@example.com>
2022-03-09 21:02:01 +0530

    commiting the file after stashing

--- Pacman/player.py
+++ Pacman/player.py
@@ -33,3 +33,80 @@ class Player(pygame.sprite.Sprite):
         self.player_image = pygame.image.load(filename).convert()
         self.player_image.set_colorkey(BLACK)
 
+    def update(self,horizontal_blocks,vertical_blocks):
+        if not self.explosion:
+            if self.rect.right < 0:
+                self.rect.left = SCREEN_WIDTH
+            elif self.rect.left > SCREEN_WIDTH:
+                self.rect.right = 0
+            if self.rect.bottom < 0:
+                self.rect.top = SCREEN_HEIGHT
+            elif self.rect.top > SCREEN_HEIGHT:
+                self.rect.bottom = 0
+            self.rect.x += self.change_x
+            self.rect.y += self.change_y
+
+            # This will stop the user for go up or down when it is inside of the box
+
+            for block in pygame.sprite.spritecollide(self,horizontal_blocks,False):
+                self.rect.centery = block.rect.centery
+                self.change_y = 0
+            for block in pygame.sprite.spritecollide(self,vertical_blocks,False):
+                self.rect.centerx = block.rect.centerx
+                self.change_x = 0
+
+            # This will cause the animation to start
+
+            if self.change_x > 0:
+                self.move_right_animation.update(10)
+                self.image = self.move_right_animation.get_current_image()
+            elif self.change_x < 0:
+                self.move_left_animation.update(10)
+                self.image = self.move_left_animation.get_current_image()
+
+            if self.change_y > 0:
+                self.move_down_animation.update(10)
+                self.image = self.move_down_animation.get_current_image()
+            elif self.change_y < 0:
+                self.move_up_animation.update(10)
+                self.image = self.move_up_animation.get_current_image()
+        else:
+            if self.explosion_animation.index == self.explosion_animation.get_length() -1:
+                pygame.time.wait(500)
+                self.game_over = True
+            self.explosion_animation.update(12)
+            self.image = self.explosion_animation.get_current_image()
+
+
+    def move_right(self):
+        self.change_x = 3
+
+    def move_left(self):
+        self.change_x = -3
+
+    def move_up(self):
+        self.change_y = -3
+
+    def move_down(self):
+        self.change_y = 3
+
+    def stop_move_right(self):
+        if self.change_x != 0:
+            self.image = self.player_image
+        self.change_x = 0
+
+    def stop_move_left(self):
+        if self.change_x != 0:
+            self.image = pygame.transform.flip(self.player_image,True,False)
+        self.change_x = 0
+
+    def stop_move_up(self):
+        if self.change_y != 0:
+            self.image = pygame.transform.rotate(self.player_image,90)
+        self.change_y = 0
+
+    def stop_move_down(self):
+        if self.change_y != 0:
+            self.image = pygame.transform.rotate(self.player_image,270)
+        self.change_y = 0
+
